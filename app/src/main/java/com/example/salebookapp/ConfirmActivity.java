1cd3f09340258be2c26c7c8af1bbdb9e58f07995
Huynh Nguyen Bac Giang <1851050037giang@ou.edu.vn>
2021-04-21 23:47:24 +0700

    thêm confirm và nhiều thứ khác

---
+++ app/src/main/java/com/example/salebookapp/ConfirmActivity.java
@@ -0,0 +1,72 @@
+package com.example.salebookapp;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+import java.util.Properties;
+import java.util.Random;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+public class ConfirmActivity extends AppCompatActivity {
+
+    EditText edtConfirmCode, edtResult;
+    Button btnSubmit, btnResend;
+    String confirmCode;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm);
+
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setTitle("CONFIRM");
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        Anhxa();
+        confirmCode = randomCode()+"";
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(isConfirmTrue()){
+                    edtResult.setText("Thành Công");
+                }else {
+                    edtResult.setText("Khônng Thành Công");
+                }
+            }
+        } );
+    }
+
+    private void Anhxa() {
+        edtConfirmCode = findViewById(R.id.edt_code);
+        edtResult = findViewById(R.id.edt_result);
+        btnSubmit = findViewById(R.id.btn_submit);
+        btnResend = findViewById(R.id.btn_resend);
+    }
+    private int randomCode(){
+        Random ran = new Random();
+
+        int s = (int)Math.floor(Math.random()*(999999-100000+1)+100000);
+        System.out.println(s);
+        return s;
+    }
+    private  boolean isConfirmTrue(){
+        System.out.println("input: "+edtConfirmCode.getText().toString()+ edtConfirmCode.getText().toString().contains(confirmCode));
+        return edtConfirmCode.getText().toString().contains(confirmCode);
+    }
+}
--- app/src/main/java/com/example/salebookapp/HomeActivity.java
+++ app/src/main/java/com/example/salebookapp/HomeActivity.java
@@ -2,13 +2,24 @@ package com.example.salebookapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
-
+    Button btnChuyen;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnChuyen = findViewById(R.id.btn_chuyen);
+        btnChuyen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(HomeActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
