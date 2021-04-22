c0c28cf10d90377f4de8f71903fa17769769c8c6
TuDV2000 <tudv2000@gmail.com>
2021-04-22 15:15:39 +0700

    pojo account

--- app/src/test/java/pojo/account.java
+++ app/src/test/java/pojo/account.java
@@ -1,5 +1,31 @@
 package pojo;
 
 public class account {
+    private String username;
+    private String password;
+    private String loai_acc;
 
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getLoai_acc() {
+        return loai_acc;
+    }
+
+    public void setLoai_acc(String loai_acc) {
+        this.loai_acc = loai_acc;
+    }
 }
