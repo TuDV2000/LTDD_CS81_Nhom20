fb078dc8fa798cb34230dfcb534450fa8ae9e79e
TuDV2000 <tudv2000@gmail.com>
2021-05-10 23:42:45 +0700

    ẩn file test (do chưa test được)

--- app/src/test/java/com/example/salebookapp/RegistActivityTest.java
+++ app/src/test/java/com/example/salebookapp/RegistActivityTest.java
@@ -1,16 +1,35 @@
-package com.example.salebookapp;
-
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
-
-@RunWith(JUnit4.class)
-public class RegistActivityTest{
-
-    @Test
-    public void testRegistion(){
-
-    }
-}
+//package com.example.salebookapp;
+//
+//import android.content.Intent;
+//
+//import org.junit.Test;
+//import org.junit.runner.RunWith;
+//import org.robolectric.Robolectric;
+//import org.robolectric.RobolectricTestRunner;
+//import org.robolectric.RuntimeEnvironment;
+//
+//import static org.junit.Assert.*;
+//import static org.robolectric.Shadows.shadowOf;
+//
+//@RunWith(RobolectricTestRunner.class)
+//public class RegistActivityTest{
+//
+//    @Test
+//    public void testRegistionIsTrue(){
+//        RegistActivity activity = Robolectric.setupActivity(RegistActivity.class);
+//
+//        activity.edtFullName.setText("");
+//        activity.edtUserName.setText("");
+//        activity.edtPassword.setText("");
+//        activity.edtConfirmPass.setText("");
+//        activity.edtCellPhone.setText("");
+//        activity.edtAddress.setText("");
+//
+//        activity.btnCreate.performClick();
+//
+//        Intent expectedIntent = new Intent(activity, ConfirmActivity.class);
+//        Intent actual = shadowOf(RuntimeEnvironment.application).getNextStartedActivity();
+//
+//        assertEquals(expectedIntent.getComponent(), actual.getComponent());
+//    }
+//}
