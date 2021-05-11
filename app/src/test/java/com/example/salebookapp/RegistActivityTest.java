fa07a3e5c57b054293d38fffb228f3df90c5ecc5
Huynh Nguyen Bac Giang <1851050037giang@ou.edu.vn>
2021-05-11 10:44:13 +0700

    give me some hope

--- app/src/test/java/com/example/salebookapp/RegistActivityTest.java
+++ app/src/test/java/com/example/salebookapp/RegistActivityTest.java
@@ -1,16 +1,25 @@
 package com.example.salebookapp;
 
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.android.controller.ActivityController;
 
-@RunWith(JUnit4.class)
-public class RegistActivityTest{
-
+import static org.junit.Assert.*;
+@RunWith(RobolectricTestRunner.class)
+public class RegistActivityTest {
+    private RegistActivity registActivity = null;
+    String text;
+    @Before
+    public void setUp() throws Exception {
+        ActivityController<RegistActivity> activityActivityController = Robolectric.buildActivity(RegistActivity.class).create().resume().visible();
+        registActivity = activityActivityController.get();
+    }
     @Test
-    public void testRegistion(){
-
+    public void test() throws Exception{
+        text = registActivity.txtChangeLogin.getText().toString();
+        assertEquals("swdwd",text);
     }
 }
